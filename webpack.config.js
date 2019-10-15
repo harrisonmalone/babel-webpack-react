15234e278d1bb6f8d76903f43a6c6c8e038d6b30
Harrison Malone <harrison.malone2@gmail.com>
2019-10-15 14:41:58 +1100

    edits

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/App.js',
+  entry: './src/HistoryData.js',
   output: {
       filename: 'main.js',
       publicPath: 'dist'
